48f12e45e22d5506899aafaf65bb73fbb7b46768
Luciano Remes <cybergenik@gmail.com>
2020-01-26 13:01:37 -0700

    fixes actually

--- htdocs/happy-birthday.php
+++ htdocs/happy-birthday.php
@@ -1,15 +1,12 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+     <title>Assignment1</title>
 </head>
 <body>
     <?php
-    echo "<h1>Happy Birthday</h1>"
-    echo "<img src="img/image.png" alt="shitty birthday cake"> 
+    echo "<h1>Happy Birthday</h1>";
+    echo "<img src='img/image.png' alt='shitty birthday cake'>"; 
     ?>
 </body>
 </html>
