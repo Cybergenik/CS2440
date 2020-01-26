b1fee3b682989a67177d8c45af73600803010abd
Luciano Remes <cybergenik@gmail.com>
2020-01-25 20:58:57 -0700

    fixing

--- web/index.php
+++ web/index.php
@@ -4,5 +4,7 @@
   <body>
     <h1>This is Luciano Remes' PHP page</h1>
     <p>Throughout the semester, various PHP projects will be added to this page.</p>
+
+    <p>Assignment1: <a href="happy-birthday/index.php">Happy Birthday</a></p>
   </body>
 </html>
