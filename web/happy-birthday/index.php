4e133cd7fbeb1f3418e69b7558610fafc4e8b4ec
Luciano Remes <cybergenik@gmail.com>
2020-01-25 20:45:16 -0700

    Assignment1-Happy Birthday

---
+++ web/happy-birthday/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    echo "<h1>Happy Birthday</h1>"
+    echo "<img src="img/image.jpg" alt="shitty birthday cake"> 
+    ?>
+</body>
+</html>
