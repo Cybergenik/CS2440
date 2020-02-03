1b284ba97cc94215d62af010bcc05315dde5c445
Luciano Remes <cybergenik@gmail.com>
2020-02-02 23:42:02 -0700

    stuff

--- htdocs/music-database/music.php
+++ htdocs/music-database/music.php
@@ -6,18 +6,18 @@
   <body id='bg'>
     <h1>Music Albums I like:</h1>
    <?php
-     $arr = array("The Black Parade By My Chemical Romance", "American Idiot By Green Day", "Dookie By Green Day", "21st Century Breakdown By Green Day", "Nimrod By Green Day", "Night at the Opera By Queen", "Innuendo By Queen", "Nevermind by Nirvana", "In Utero by Nirvana", "Abbey Road By The Beatles");
-     echo "<table>"
-     echo "<tr>"
-      echo "<th>Album:</th>"
-      echo "</tr>"
+     $album = array("The Black Parade By My Chemical Romance", "American Idiot By Green Day", "Dookie By Green Day", "21st Century Breakdown By Green Day", "Nimrod By Green Day", "Night at the Opera By Queen", "Innuendo By Queen", "Nevermind by Nirvana", "In Utero by Nirvana", "Abbey Road By The Beatles");
+     echo "<table>";
+     echo "<tr>";
+     echo "<th>Album:</th>";
+     echo "</tr>";
      foreach ($album as $i) 
      {
-      echo "<tr>"
-      echo "<td>$i</td>"
-      echo "</tr>"
+      echo "<tr>";
+      echo "<td>$i</td>";
+      echo "</tr>";
      }
-     echo "</table>"
+     echo "</table>";
    ?>
 </body>
 </html>
