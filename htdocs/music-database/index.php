86347a28e95832b20ce24fcf91aedb845118b6c4
Luciano Remes <cybergenik@gmail.com>
2020-02-02 21:26:51 -0700

    stuff

---
+++ htdocs/music-database/index.php
@@ -0,0 +1,8 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head> Luciano Remes </head>
+  <body>
+    <h1>Assignment #2</h1>
+    <p>Music<a href="music.php"></a></p>
+  </body>
+</html>
---
+++ htdocs/music-database/music.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head> Luciano Remes </head>
+  <body>
+    <h1>Music Albums I like:</h1>
+   <?php
+   	$arr = array("  ");
+   ?>
+</body>
+</html>
