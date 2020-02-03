e7ce4ee803ded42ac47cbf4d7aa0c7e5ec2ea9d0
Luciano Remes <cybergenik@gmail.com>
2020-02-02 23:27:39 -0700

    stuff

--- htdocs/music-database/index.php
+++ htdocs/music-database/index.php
@@ -1,8 +1,10 @@
 <!DOCTYPE html>
 <html lang="en">
-  <head> Luciano Remes </head>
-  <body>
+  <head>
+  <link rel="stylesheet" href="css/main.css">
+  </head>
+  <body id='bg'>
     <h1>Assignment #2</h1>
-    <p>Music<a href="music.php"></a></p>
+    <a class="mybutton" href="#about">Music Albums</a>
   </body>
 </html>
--- htdocs/music-database/music.php
+++ htdocs/music-database/music.php
@@ -1,10 +1,23 @@
 <!DOCTYPE html>
 <html lang="en">
-  <head> Luciano Remes </head>
-  <body>
+  <head>
+  <link rel="stylesheet" href="css/main.css">
+  </head>
+  <body id='bg'>
     <h1>Music Albums I like:</h1>
    <?php
-   	$arr = array("  ");
+     $arr = array("The Black Parade By My Chemical Romance", "American Idiot By Green Day", "Dookie By Green Day", "21st Century Breakdown By Green Day", "Nimrod By Green Day", "Night at the Opera By Queen", "Innuendo By Queen", "Nevermind by Nirvana", "In Utero by Nirvana", "Abbey Road By The Beatles");
+     echo "<table>"
+     echo "<tr>"
+      echo "<th>Album:</th>"
+      echo "</tr>"
+     foreach ($album as $i) 
+     {
+      echo "<tr>"
+      echo "<td>$i</td>"
+      echo "</tr>"
+     }
+     echo "</table>"
    ?>
 </body>
 </html>
