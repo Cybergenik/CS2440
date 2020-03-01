c032f39b595086cdd1faa093ee4cbc62eaa00020
Luciano Remes <cybergenik@gmail.com>
2020-03-01 02:44:03 -0700

    adding link

--- htdocs/index.php
+++ htdocs/index.php
@@ -8,6 +8,6 @@
     <h1>This is Luciano Remes' PHP page</h1>
     <p>Throughout the semester, various PHP projects will be added to this page.</p>
     <a class="mybutton" href="happy-birthday/index.php">Happy Birthday</a>
-    <a class="mybutton" href="happy-birthday/index.php">Full Music Site</a>
+    <a class="mybutton" href="music-database/index.php">Full Music Site</a>
   </body>
 </html>
