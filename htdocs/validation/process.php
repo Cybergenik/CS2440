3a8c8863bc14b4bc3a97fd2e6fb16627d4388c2b
Luciano Remes <cybergenik@gmail.com>
2020-03-01 19:06:55 -0700

    error handling

--- htdocs/validation/process.php
+++ htdocs/validation/process.php
@@ -14,13 +14,13 @@
         else{
            header('location: index.php?email='.$email.'&phone='.$phone);
             if (empty($_POST['email'])){
-            echo "<script>alert('Please fill out the email field')</script>";
+            echo "<h4 style='font-color: red'>Please fill out the email field</h4>";
             }
             elseif (empty($_POST['phone'])){
-                echo "<script>alert('Please fill out the phone field')</script>";
+                echo "<h4 style='font-color: red'>Please fill out the phone field</h4>";
             }
             else{
-                echo "<script>alert('Please fill out all fields')</script>";
+                echo "<h4 style='font-color: red'>Please fill out all fields</h4>";
             }
         }
 
