5c111019935127cb3f2e732710b77e0ebf09fc81
Luciano Remes <cybergenik@gmail.com>
2020-03-01 19:01:24 -0700

    changes

--- htdocs/validation/index.php
+++ htdocs/validation/index.php
@@ -1,11 +1,30 @@
 <!DOCTYPE html>
 <html lang="en">
-  <head>
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel="stylesheet" href="css/main.css">
-  </head>
-  <body id='bg'>
-    <h1>Band Website</h1>
-    <a class="mybutton" href="music.php">Music Albums</a>
-    <a class="mybutton" href="products.php">Order a T-Shirt</a>
-  </body>
+  <title>Data Validation</title>
+</head>
+<body id="bg">
+  <?php
+    function derror(){
+      echo "<script>alert('Please fill out fields')</script>";
+    }
+    if (isset($_GET['err']))
+      derror();
+    if (isset($_GET['email'])) $evalue = $_GET['email']; else $evalue = '';
+    if (isset($_GET['phone'])) $pvalue = $_GET['phone']; else $pvalue = '';
+
+    echo '<h1>Data Validation</h1>';
+    echo '<form class="flex-container" action="process.php" method="post">';
+    echo '    <input class="myin" class="input" name="email" id="email" type="email" placeholder="Email" pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2, 4}$" value="'. $evalue .'">';
+    echo '    <input class="myin" class="input" name="phone" id="phone" type="tel" placeholder="Phone Number " pattern="[0-9]{3}[0-9]{3}[0-9]{4}" value="'. $pvalue .'">';
+    echo '    <div class="flex-container2">';
+    echo '    <input class="mybutton" type="reset">';
+    echo '    <input class="mybutton" type="submit">';
+    echo '    </div>';
+    echo '</form>';
+  ?>
+</body>
 </html>
---
+++ htdocs/validation/process.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        if (!empty($_POST['email']) && !empty($_POST['phone'])){
+            $email = $_POST['email'];
+            $phone = $_POST['phone'];
+        }
+        else{
+           header('location: index.php?err=1&email='.$email.'&phone='.$phone);
+            if (empty($_POST['email'])){
+            echo "<script>alert('Please fill out the email field')</script>";
+            }
+            elseif (empty($_POST['phone'])){
+                echo "<script>alert('Please fill out the phone field')</script>";
+            }
+            else{
+                echo "<script>alert('Please fill out all fields')</script>";
+            }
+        }
+
+    ?>
+</body>
+</html>
