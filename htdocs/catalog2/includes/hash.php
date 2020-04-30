cb552eab5db22052c7ba7825481cde4d70195355
Luciano Remes <cybergenik@gmail.com>
2020-04-30 17:34:14 -0600

    removing trash hashing function

--- htdocs/catalog2/includes/hash.php
+++
@@ -1,9 +0,0 @@
-<?php
-    function hasher($u, $pass){
-        $salt1 = hash("SHA512", $u);
-        $salt2 = hash("SHA512", $u.$u);
-        $hpass = $salt1.$pass.$salt2;
-        $hpass = hash("SHA512", $hpass);
-        return $hpass;
-    }
-?>
